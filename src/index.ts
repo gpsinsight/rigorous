7bd0aef104dc78af01d4827c601944f6e84c539c
Steve Konves <steve.konves@gpsinsight.com>
2019-02-18 08:06:03 -0700

    refactor: clean up index

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,2 @@
-import { serve } from './server';
-import { watch } from './watcher';
-
-watch('/Users/skonves/code/api-specs/.resolved', { port: 8800 }).catch(
-  console.error,
-);
+export { serve } from './server';
+export { watch } from './watcher';
